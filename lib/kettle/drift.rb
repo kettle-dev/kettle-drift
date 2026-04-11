a425b87caf06eae0e9077c46f77977b166ec5b24
Peter H. Boling <peter.boling@gmail.com>
2026-04-11 17:26:51 -0600

    🎨 Apply kettle-jem template v1.0.0

--- lib/kettle/drift.rb
+++ lib/kettle/drift.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require "version_gem"
 require_relative "drift/version"
 
+Kettle::Drift::Version.class_eval do
+  extend VersionGem::Basic
+end
 module Kettle
   module Drift
     class Error < StandardError; end
