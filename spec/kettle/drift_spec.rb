bd038cd14dc083203b58f8fee359e63d6feeaaca
Peter H. Boling <peter.boling@gmail.com>
2026-04-11 17:15:23 -0600

    🎉 Tada!

---
+++ spec/kettle/drift_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Kettle::Drift do
+  it "has a version number" do
+    expect(Kettle::Drift::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
